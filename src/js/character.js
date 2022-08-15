6d9fb23033aa4173979c4fb2aa6c188c24005539
comradexlight <comradexlight@gmail.com>
2022-08-15 21:33:57 +0300

    initial commit

---
+++ src/js/character.js
@@ -0,0 +1,10 @@
+class Character {
+	constructor(name, type, health, level, attack, defence) {
+		this.name = name;
+		this.type = String(type);
+		this.health = health;
+		this.level = level;
+		this.attack = attack;
+		this.defence = defence;
+	}
+}
